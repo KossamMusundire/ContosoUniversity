693eb13d62a767d5ca898d81f58df3686c5e8d27
MSNKOS001 <MSNKOS001@COM011>
2025-04-04 17:24:00 +0200

    Add project files.

---
+++ Contoso University/Pages/Students/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ContosoUniversity.Models;
+using Contoso_University.Data;
+
+namespace Contoso_University.Pages.Students
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Contoso_University.Data.SchoolContext _context;
+
+        public CreateModel(Contoso_University.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Student Student { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Student.Add(Student);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Contoso University/Pages/Students/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+using Contoso_University.Data;
+
+namespace Contoso_University.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Contoso_University.Data.SchoolContext _context;
+
+        public IndexModel(Contoso_University.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Student { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Student = await _context.Student.ToListAsync();
+        }
+    }
+}
